fbba117bdfa865b614f1491fedf8fc8784873860
crogan <>
2011-07-04 17:39:59 +0000

    RooDalglish

--- src/Linkdef.h
+++ src/Linkdef.h
@@ -14,6 +14,7 @@
 #pragma link C++ defined_in "RooTwoSideGaussianWithAnExponentialTail.h";
 #pragma link C++ defined_in "RooTwoSideGaussianWithTwoExponentialTails.h";
 #pragma link C++ defined_in "RooRazor2DTail.h";
+#pragma link C++ defined_in "RooDalglish.h";
 
 #endif
 
