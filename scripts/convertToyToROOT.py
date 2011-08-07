d3cf70361e47d559a29c8fbfadab6eb7ee5f1325
mpierini <>
2011-08-07 20:19:30 +0000

    *** empty log message ***

--- scripts/convertToyToROOT.py
+++ scripts/convertToyToROOT.py
@@ -6,14 +6,13 @@ if __name__ == '__main__':
     
     MR = rt.RooRealVar("MR", "MR", 300., 7000.)
     Rsq = rt.RooRealVar("Rsq", "Rsq", 0.09, 0.5)
-    Box = rt.RooRealVar("Box", "Box", -0.5, 5.5)
     # to add the category
     argc = len(sys.argv)
     for i in range(1,argc):
         tree = rt.TTree()
-        tree.ReadFile(sys.argv[i],"MR/D:Rsq/D:Box/I")
+        tree.ReadFile(sys.argv[i],"MR/D:Rsq/D")
         if tree.GetEntries() <1: continue
-        mydata =  rt.RooDataSet("RMRTree","RMRTree",tree, rt.RooArgSet(MR, Rsq, Box))
+        mydata =  rt.RooDataSet("RMRTree","RMRTree",tree, rt.RooArgSet(MR, Rsq))
         myfile = rt.TFile.Open("toyMC_%i.root" %i, "recreate")
         mydata.Write()
         myfile.Close()
--- scripts/expectedYield.py
+++ scripts/expectedYield.py
@@ -14,38 +14,40 @@ if __name__ == '__main__':
     hadSigRegions.append(["hS2", 1000., 1500., 0.20, 0.50])
     hadSigRegions.append(["hS3",  800., 1500., 0.30, 0.50])
     hadSigRegions.append(["hS4",  450.,  800., 0.45, 0.50])
-    hadSigRegions.append(["hS5",  350.,  500., 0.49, 0.50])
-    hadSigRegions.append(["hC1",  650., 7000., 0.09, 0.20])
-    hadSigRegions.append(["hC2",  450., 1000., 0.20, 0.30])
-    hadSigRegions.append(["hC3",  350.,  800., 0.30, 0.45])
-    hadSigRegions.append(["hC4",  350.,  450., 0.45, 0.49])
+    hadSigRegions.append(["hS5",  350.,  450., 0.49, 0.50])
+    #hadSigRegions.append(["hC1",  650., 7000., 0.09, 0.20])
+    #hadSigRegions.append(["hC2",  450., 1000., 0.20, 0.30])
+    #hadSigRegions.append(["hC3",  350.,  800., 0.30, 0.45])
+    #hadSigRegions.append(["hC4",  350.,  450., 0.45, 0.49])
 
-    Boxes = ["Mu", "Ele", "MuMu", "MuEle", "EleEle"]
+    Box = sys.argv[1]
+    Regions = sigRegions
+    if Box == "Had" or  Box == "Ele" or  Box == "Mu": Regions = hadSigRegions
     histos = []
-    for Box in Boxes:
-        Regions = sigRegions
-        if Box == Had: hadSigRegions = sigRegions
-        for Region in Regions:
-            histos.append([Box, Region, rt.TH1D("%s_%s" %(Region, Box), "%s_%s" %(Region, Box), 2000, 0., 2000.)])
-    
+    for Region in Regions:
+        if Region[0].find("hS") != -1:
+            histos.append([Box, Region, rt.TH1D("%s_%s" %(Region[0], Box), "%s_%s" %(Region[0], Box), 80, 0., 80.)])
+        else:
+            histos.append([Box, Region, rt.TH1D("%s_%s" %(Region[0], Box), "%s_%s" %(Region[0], Box), 1000, 0., 1000.)])
+            
     treeName = "RMRTree"
     argc = len(sys.argv)
-    for i in range(1,argc):
-        myfile = rt.TFile(sys,argv[i])
+    for i in range(2,argc):
+        myfile = rt.TFile(sys.argv[i])
         gdata = myfile.Get(treeName)
         if gdata == None: continue
         if gdata.InheritsFrom("RooDataSet") != True: continue
-        for histos in histo:
-            histos[2].Fill(gdata.reduce("MR>%f" %histos[1].[0]).reduce("MR<=%f" %histos[1].[1]).reduce("Rsq>%f" %histos[1].[2]).reduce("Rsq<=%f" %histos[1].[3]))
+        for histo in histos:
+            histo[2].Fill(gdata.reduce("MR>%s" %histo[1][1]).reduce("MR<=%s" %histo[1][2]).reduce("Rsq>%s" %histo[1][3]).reduce("Rsq<=%s" %histo[1][4]).numEntries())
             continue
         continue
 
     print "Box        Expected Range"
     # print the result and write it to out
-    fileOut = rt.Tfile.Open("expectedYield.root", "recreate")
-    for histos in histo:
-        print "%s        %f +/- %f" %(histos[0], histos[2].GetMean(), histos[2].GetRMS())
-        histos[2].Write()
+    fileOut = rt.TFile.Open("expectedYield_%s.root" %Box, "recreate")
+    for histo in histos:
+        print "%s_%s        %f +/- %f" %(histo[1][0], histo[0], histo[2].GetMean(), histo[2].GetRMS())
+        histo[2].Write()
     fileOut.Close()
 
         
