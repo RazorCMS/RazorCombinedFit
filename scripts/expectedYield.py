e694e6e586c7d9335e19c85959a789cf5360e057
woodson <>
2011-08-08 15:26:50 +0000

    This commit was manufactured by cvs2svn to create tag 'wreece_080811'.
    Sprout from master 2011-08-08 15:26:49 UTC wreece 'adds signal sigmas'
    Cherrypick from master 2011-08-07 20:19:30 UTC mpierini '*** empty log message ***':
        scripts/expectedYield.py
    Delete:
        scripts/observedYield.py

--- scripts/expectedYield.py
+++ scripts/expectedYield.py
@@ -10,31 +10,19 @@ if __name__ == '__main__':
     sigRegions.append(["S3", 350.,  450., 0.30, 0.50])
 
     hadSigRegions = []
-    hadSigRegions.append(["hS1", 1500., 7000., 0.16, 0.50])
+    hadSigRegions.append(["hS1", 1500., 7000., 0.09, 0.50])
     hadSigRegions.append(["hS2", 1000., 1500., 0.20, 0.50])
     hadSigRegions.append(["hS3",  800., 1500., 0.30, 0.50])
     hadSigRegions.append(["hS4",  450.,  800., 0.45, 0.50])
-    hadSigRegions.append(["hS5",  400.,  450., 0.49, 0.50])
+    hadSigRegions.append(["hS5",  350.,  450., 0.49, 0.50])
     #hadSigRegions.append(["hC1",  650., 7000., 0.09, 0.20])
     #hadSigRegions.append(["hC2",  450., 1000., 0.20, 0.30])
     #hadSigRegions.append(["hC3",  350.,  800., 0.30, 0.45])
     #hadSigRegions.append(["hC4",  350.,  450., 0.45, 0.49])
 
-    lepSigRegions = []
-    lepSigRegions.append(["hS1", 1500., 7000., 0.09, 0.50])
-    lepSigRegions.append(["hS2", 1000., 1500., 0.20, 0.50])
-    lepSigRegions.append(["hS3",  800., 1500., 0.30, 0.50])
-    lepSigRegions.append(["hS4",  450.,  800., 0.45, 0.50])
-    lepSigRegions.append(["hS5",  350.,  450., 0.49, 0.50])
-    #lepSigRegions.append(["hC1",  650., 7000., 0.09, 0.20])
-    #lepSigRegions.append(["hC2",  450., 1000., 0.20, 0.30])
-    #lepSigRegions.append(["hC3",  350.,  800., 0.30, 0.45])
-    #lepSigRegions.append(["hC4",  350.,  450., 0.45, 0.49])
-
     Box = sys.argv[1]
     Regions = sigRegions
-    if Box == "Had" : Regions = hadSigRegions
-    if Box == "Ele" or  Box == "Mu": Regions = lepSigRegions
+    if Box == "Had" or  Box == "Ele" or  Box == "Mu": Regions = hadSigRegions
     histos = []
     for Region in Regions:
         if Region[0].find("hS") != -1:
