4b0dc266e156cc34f985f7aaaffdeaaa23d64fac
lykken <>
2011-08-22 18:03:14 +0000

    *** empty log message ***

--- scripts/runMDLimit.py
+++ scripts/runMDLimit.py
@@ -26,22 +26,22 @@ if __name__ == '__main__':
         M0 = int(signal.split('_')[-2].split('-')[-1])
         M12 = int(signal.split('_')[-1].split('-')[-1])
 
-        if float(M12) < 500-0.4*M0: continue
-        if float(M12) > 750-0.25*M0: continue
+        #if float(M12) < 500-0.4*M0: continue
+        #if float(M12) > 750-0.25*M0: continue
 
         print "\nNow scanning mSUGRA (M0,M12)=("+str(M0)+","+str(M12)+")\n"
 
         for i in range(options.iJob,options.iJob+1):
 
             # prepare the script to run 
-            outputname = "%s_%i.src" %(signal,i)
+            outputname = "%s_%s_%i.src" %(signal,options.box,i)
             outputfile = open(outputname,'w')
             outputfile.write('#!/bin/bash\n')
             outputfile.write('cd '+pwd+'\n')
             outputfile.write("eval `scramv1 run -sh`\n")
             outputfile.write("source setup.sh\n")
-            mydir = "/tmp/smaria/%s_%i" %(signal,i)
-            outputfile.write("rm /tmp/smaria/*\n")
+            mydir = "/tmp/lykken/%s_%i" %(signal,i)
+            outputfile.write("rm /tmp/lykken/*\n")
             outputfile.write("mkdir %s\n" % mydir)
             outputfile.write("cp %s %s \n" %(signalpath, mydir))
             # convert original signal file to box-by-box datasets
@@ -67,8 +67,8 @@ if __name__ == '__main__':
             # perform limit toys(bkgd only) setting fits
             outputfile.write("python scripts/runAnalysis.py -a SingleBoxFit -s %i -c config_summer11/SingleBoxFit_Prompt_fR1fR2fR3fR4_SignalTemplate.cfg -o %s/LimitBkgToys_MR%s_R%s_%s_%s_%i.root -i /afs/cern.ch/user/m/mpierini/public/RAZORFITS/%sBox_NOMINAL.root %s/%s_MR%s_R%s_%s.root -b --limit -e >& /dev/null \n" %(seed,mydir,MR,R,signal,box,i,box,mydir,signal,MR,R,box))
             # copy output files
-            outputfile.write("scp %s/LimitBkgSigToys_MR%s_R%s_%s_%s_%i.root smaria@lxcms132:/data1/smaria/SIGNALMODELTOYS/\n" %(mydir,MR,R,signal,box,i))
-            outputfile.write("scp %s/LimitBkgToys_MR%s_R%s_%s_%s_%i.root smaria@lxcms132:/data1/smaria/SIGNALMODELTOYS/\n" %(mydir,MR,R,signal,box,i))
+            outputfile.write("scp %s/LimitBkgSigToys_MR%s_R%s_%s_%s_%i.root lykken@lxcms132:/data1/lykken/SIGNALMODELTOYS/\n" %(mydir,MR,R,signal,box,i))
+            outputfile.write("scp %s/LimitBkgToys_MR%s_R%s_%s_%s_%i.root lykken@lxcms132:/data1/lykken/SIGNALMODELTOYS/\n" %(mydir,MR,R,signal,box,i))
             # remove output files
             outputfile.write("rm -r %s\n" %(mydir))
             outputfile.close
