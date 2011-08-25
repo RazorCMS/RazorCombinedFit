467716f9feaec55e6f9b621438f40ec1a2f71664
mpierini <>
2011-08-25 20:54:13 +0000

    *** empty log message ***

--- python/RazorCombinedFit/Framework/Box.py
+++ python/RazorCombinedFit/Framework/Box.py
@@ -490,6 +490,138 @@ class Box(object):
 
         return [histoData,histoToy,c]
 
+    def plot1DHistoAllComponents(self, inputFile, xvarname, nbins = 25, ranges=None, data = None):
+        
+        rangeNone = False
+        if ranges is None:
+            rangeNone = True
+            ranges = ['']
+            
+        #before I find a better way
+        rangeCut = self.getVarRangeCutNamed(ranges=ranges)
+        if data is None:
+            data = RootTools.getDataSet(inputFile,'RMRTree', self.cut)
+            data = data.reduce(rangeCut)
+        toyData = self.workspace.pdf(self.fitmodel).generate(self.workspace.set('variables'), 50*data.numEntries())
+        toyData = toyData.reduce(self.getVarRangeCutNamed(ranges=ranges))
+
+        if self.name != "MuEle":
+            # no ttbar
+            Ntt = self.workspace.var("Ntot_TTj").getVal()
+            self.workspace.var("Ntot_TTj").setVal(0.)
+            # no znn+jets [this is ONLY for the Had box]
+            Nznn = 0
+            if self.name == "Had":
+                Nznn = self.workspace.var("Ntot_Znn").getVal()
+                self.workspace.var("Ntot_Znn").setVal(0.)
+            
+            toyDataWln = self.workspace.pdf(self.fitmodel).generate(self.workspace.set('variables'), int(50*(data.numEntries()-Ntt-Nznn)))
+            toyDataWln = toyDataWln.reduce(self.getVarRangeCutNamed(ranges=ranges))
+            if self.name == "Had":
+                Nwj = self.workspace.var("Ntot_Wln").getVal()
+                self.workspace.var("Ntot_Wln").setVal(0.)
+                self.workspace.var("Ntot_Znn").setVal(Nznn)
+                toyDataZnn = self.workspace.pdf(self.fitmodel).generate(self.workspace.set('variables'), int(50*(data.numEntries()-Ntt-Nwj)))
+                toyDataZnn = toyDataZnn.reduce(self.getVarRangeCutNamed(ranges=ranges))
+                self.workspace.var("Ntot_Wln").setVal(Nwj)
+                
+            self.workspace.var("Ntot_TTj").setVal(Ntt)
+
+        xmin = min([self.workspace.var(xvarname).getMin(r) for r in ranges])
+        xmax = max([self.workspace.var(xvarname).getMax(r) for r in ranges])
+
+        # define 1D histograms
+        histoData = rt.TH1D("histoData", "histoData",nbins, xmin, xmax)
+        histoToy = rt.TH1D("histoToy", "histoToy",nbins, xmin, xmax)
+        histoToyTTj = rt.TH1D("histoToyTTj", "histoToyTTj",nbins, xmin, xmax)
+        histoToyWln = rt.TH1D("histoToyWln", "histoToyWln",nbins, xmin, xmax)
+        histoToyZnn = rt.TH1D("histoToyZnn", "histoToyZnn",nbins, xmin, xmax)
+
+        def setName(h, name):
+            h.SetName('%s_%s_%s_ALLCOMPONENTS' % (h.GetName(),name,'_'.join(ranges)) )
+            h.GetXaxis().SetTitle(name)
+        
+        def SetErrors(histo, nbins):
+            for i in range(1, nbins+1):
+                histo.SetBinError(i,rt.TMath.Sqrt(histo.GetBinContent(i)))
+
+        # project the data on the histograms
+        data.tree().Project("histoData",xvarname)
+        toyData.tree().Project("histoToy",xvarname)
+        scaleFactor = histoData.Integral()/histoToy.Integral()
+        if self.name != "MuEle":
+            toyDataWln.tree().Project("histoToyWln",xvarname)
+            toyData.tree().Project("histoToyTTj",xvarname)
+            histoToyTTj.Add(histoToyWln, -1)
+            if self.name == "Had":
+                toyDataZnn.tree().Project("histoToyZnn",xvarname)
+                histoToyTTj.Add(histoToyZnn, -1)
+                histoToyZnn.Scale(scaleFactor)
+                SetErrors(histoToyZnn, nbins)
+                setName(histoToyZnn,xvarname)
+                histoToyZnn.SetLineColor(rt.kGreen)    
+            histoToyTTj.Scale(scaleFactor)
+            histoToyWln.Scale(scaleFactor)
+            SetErrors(histoToyTTj, nbins)
+            SetErrors(histoToyWln, nbins)
+            setName(histoToyTTj,xvarname)
+            setName(histoToyWln,xvarname)
+            histoToyTTj.SetLineColor(rt.kOrange)
+            histoToyTTj.SetLineWidth(2)
+            if self.name == "EleEle" or self.name == "MuMu": histoToyWln.SetLineColor(rt.kMagenta)
+            else: histoToyWln.SetLineColor(rt.kRed)
+            histoToyWln.SetLineWidth(2)
+
+        histoToy.Scale(scaleFactor)
+        SetErrors(histoToy, nbins)
+        setName(histoData,xvarname)
+        setName(histoToy,xvarname)
+        histoData.SetMarkerStyle(20)
+        histoToy.SetLineColor(rt.kBlue)
+        histoToy.SetLineWidth(2)
+
+        c = rt.TCanvas()
+        c.SetName('DataMC_%s_%s_ALLCOMPONENTS' % (xvarname,'_'.join(ranges)) )
+        histoData.Draw("pe")
+
+        if self.name != "MuEle":
+            if self.name == "Had":
+                histoToyZnn.DrawCopy("histsame")
+                histoToyZnn.SetFillColor(rt.kGreen)
+                histoToyZnn.SetFillStyle(3018)
+                histoToyZnn.Draw('e2same')            
+            histoToyWln.DrawCopy("histsame")
+            if self.name == "EleEle" or self.name == "MuMu": histoToyWln.SetFillColor(rt.kMagenta)
+            else: histoToyWln.SetFillColor(rt.kRed)
+            histoToyWln.SetFillStyle(3018)
+            histoToyWln.Draw('e2same')
+            histoToyTTj.DrawCopy('histsame')
+            histoToyTTj.SetFillColor(rt.kOrange)
+            histoToyTTj.SetFillStyle(3018)
+            histoToyTTj.Draw('e2same')        
+            histoToy.DrawCopy('histsame')
+
+        histoToy.DrawCopy('histsame')
+        histoToy.SetFillColor(rt.kBlue)
+        histoToy.SetFillStyle(3018)
+        histoToy.Draw('e2same')
+
+        #histoData.Draw("pesame")
+
+        #leg = rt.TLegend(0.6,0.6,0.9,0.9)
+        #leg.SetFillColor(0)
+        #leg.AddEntry(histoToyWln.GetName(),"W+jets","l")
+        #leg.AddEntry(histoToyTTj.GetName(),"t#bar{t}","l")
+        #leg.AddEntry(histoToy.GetName(),"Total","l")
+        #leg.Draw()
+
+        histToReturn = [histoToy, histoData, c]
+        if self.name != "MuEle":
+            histToReturn.append(histoToyTTj)
+            histToReturn.append(histoToyWln)
+            if self.name == "Had": histToReturn.append(histoToyZnn)
+
+        return histToReturn
 
     def plot(self, inputFile, store, box):
         [store.store(p, dir = box) for p in self.plotObservables(inputFile)]
