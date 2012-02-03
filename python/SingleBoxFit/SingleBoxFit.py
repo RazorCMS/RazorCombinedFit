034bc19f1ee53a4d1f16648f84acf8751f3155e9
wreece <>
2012-02-03 16:21:53 +0000

    Append should have been merge

--- python/SingleBoxFit/SingleBoxFit.py
+++ python/SingleBoxFit/SingleBoxFit.py
@@ -33,17 +33,17 @@ class SingleBoxAnalysis(Analysis.Analysis):
             else: boxes[box] = RazorBox.RazorBox(box, self.config.getVariables(box, "variables"))
             self.config.getVariablesRange(box,"variables" ,boxes[box].workspace)
             # Wln
-            boxes[box].defineSet("pdf1pars_Wln", self.config.getVariables(box, "pdf1_Wln"))
-            boxes[box].defineSet("pdf2pars_Wln", self.config.getVariables(box, "pdf2_Wln"))
-            boxes[box].defineSet("otherpars_Wln", self.config.getVariables(box, "others_Wln"))
+            #boxes[box].defineSet("pdf1pars_Wln", self.config.getVariables(box, "pdf1_Wln"))
+            #boxes[box].defineSet("pdf2pars_Wln", self.config.getVariables(box, "pdf2_Wln"))
+            #boxes[box].defineSet("otherpars_Wln", self.config.getVariables(box, "others_Wln"))
             # Zll
-            boxes[box].defineSet("pdf1pars_Zll", self.config.getVariables(box, "pdf1_Zll"))
-            boxes[box].defineSet("pdf2pars_Zll", self.config.getVariables(box, "pdf2_Zll"))
-            boxes[box].defineSet("otherpars_Zll", self.config.getVariables(box, "others_Zll"))
+            #boxes[box].defineSet("pdf1pars_Zll", self.config.getVariables(box, "pdf1_Zll"))
+            #boxes[box].defineSet("pdf2pars_Zll", self.config.getVariables(box, "pdf2_Zll"))
+            #boxes[box].defineSet("otherpars_Zll", self.config.getVariables(box, "others_Zll"))
             # Znn
-            boxes[box].defineSet("pdf1pars_Znn", self.config.getVariables(box, "pdf1_Znn"))
-            boxes[box].defineSet("pdf2pars_Znn", self.config.getVariables(box, "pdf2_Znn"))
-            boxes[box].defineSet("otherpars_Znn", self.config.getVariables(box, "others_Znn"))
+            #boxes[box].defineSet("pdf1pars_Znn", self.config.getVariables(box, "pdf1_Znn"))
+            #boxes[box].defineSet("pdf2pars_Znn", self.config.getVariables(box, "pdf2_Znn"))
+            #boxes[box].defineSet("otherpars_Znn", self.config.getVariables(box, "others_Znn"))
             # TTj
             boxes[box].defineSet("pdf1pars_TTj", self.config.getVariables(box, "pdf1_TTj"))
             boxes[box].defineSet("pdf2pars_TTj", self.config.getVariables(box, "pdf2_TTj"))
@@ -90,7 +90,7 @@ class SingleBoxAnalysis(Analysis.Analysis):
                 
             fitPars = study.fitParDataSet()
             outpars = rt.RooDataSet('fitPars_%s' % box, 'fitPars_%s' % box, fitPars,fitPars.get(0))
-            outpars.append(fit)
+            outpars.merge(fit)
             
             self.store(outpars, dir='%s_Toys' % box)
             
