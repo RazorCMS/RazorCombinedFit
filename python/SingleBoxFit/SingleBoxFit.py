ebb5035ae0c070fb9d9944f9e1b0dfc46ffbfbb0
mpierini <>
2012-02-04 19:43:36 +0000

    added switch for multijet

--- python/SingleBoxFit/SingleBoxFit.py
+++ python/SingleBoxFit/SingleBoxFit.py
@@ -32,18 +32,19 @@ class SingleBoxAnalysis(Analysis.Analysis):
             elif self.Analysis == "MULTIJET": boxes[box] = RazorMultiJetBox.RazorMultiJetBox(box, self.config.getVariables(box, "variables"))
             else: boxes[box] = RazorBox.RazorBox(box, self.config.getVariables(box, "variables"))
             self.config.getVariablesRange(box,"variables" ,boxes[box].workspace)
-            # Wln
-            #boxes[box].defineSet("pdf1pars_Wln", self.config.getVariables(box, "pdf1_Wln"))
-            #boxes[box].defineSet("pdf2pars_Wln", self.config.getVariables(box, "pdf2_Wln"))
-            #boxes[box].defineSet("otherpars_Wln", self.config.getVariables(box, "others_Wln"))
-            # Zll
-            #boxes[box].defineSet("pdf1pars_Zll", self.config.getVariables(box, "pdf1_Zll"))
-            #boxes[box].defineSet("pdf2pars_Zll", self.config.getVariables(box, "pdf2_Zll"))
-            #boxes[box].defineSet("otherpars_Zll", self.config.getVariables(box, "others_Zll"))
-            # Znn
-            #boxes[box].defineSet("pdf1pars_Znn", self.config.getVariables(box, "pdf1_Znn"))
-            #boxes[box].defineSet("pdf2pars_Znn", self.config.getVariables(box, "pdf2_Znn"))
-            #boxes[box].defineSet("otherpars_Znn", self.config.getVariables(box, "others_Znn"))
+            if self.Analysis != "MULTIJET":
+                # Wln
+                boxes[box].defineSet("pdf1pars_Wln", self.config.getVariables(box, "pdf1_Wln"))
+                boxes[box].defineSet("pdf2pars_Wln", self.config.getVariables(box, "pdf2_Wln"))
+                boxes[box].defineSet("otherpars_Wln", self.config.getVariables(box, "others_Wln"))
+                # Zll
+                boxes[box].defineSet("pdf1pars_Zll", self.config.getVariables(box, "pdf1_Zll"))
+                boxes[box].defineSet("pdf2pars_Zll", self.config.getVariables(box, "pdf2_Zll"))
+                boxes[box].defineSet("otherpars_Zll", self.config.getVariables(box, "others_Zll"))
+                # Znn
+                boxes[box].defineSet("pdf1pars_Znn", self.config.getVariables(box, "pdf1_Znn"))
+                boxes[box].defineSet("pdf2pars_Znn", self.config.getVariables(box, "pdf2_Znn"))
+                boxes[box].defineSet("otherpars_Znn", self.config.getVariables(box, "others_Znn"))
             # TTj
             boxes[box].defineSet("pdf1pars_TTj", self.config.getVariables(box, "pdf1_TTj"))
             boxes[box].defineSet("pdf2pars_TTj", self.config.getVariables(box, "pdf2_TTj"))
