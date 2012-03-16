768a40c89263b90d0dc6316bf0b2bb7c9223fb58
wreece <>
2012-03-16 14:50:30 +0000

    tries to fit the tail of Rsq by using a shape from a control channel.

--- python/SingleBoxFit/RazorMultiJetBox.py
+++ python/SingleBoxFit/RazorMultiJetBox.py
@@ -10,7 +10,7 @@ class RazorMultiJetBox(RazorBox.RazorBox):
         super(RazorMultiJetBox,self).__init__(name, variables)
         
         # DATA DEFAULT
-        self.zeros = {'TTj':[], 'QCD':['BJET']}
+        self.zeros = {'TTj':['CR5JSingleLeptonBVeto','CR6JSingleLeptonBVeto'], 'QCD':[]}
         # MC FIT: QCD
         #self.zeros = {'TTj':['Had','QCD'], 'QCD':[]}
         # MC FIT: TTj
@@ -46,27 +46,26 @@ class RazorMultiJetBox(RazorBox.RazorBox):
         def floatSomething(z):
             """Switch on or off whatever you want here"""
             # the "effective" first component in the Had box
-            if not (self.name == "BJET" and z == "QCD"): self.float1stComponentWithPenalty(z, True)
-            if z != "QCD": self.float2ndComponentWithPenalty(z, True)
+            self.float1stComponentWithPenalty(z, True)
+            self.float2ndComponentWithPenalty(z, True)
             self.floatYield(z)
             self.floatFraction(z)
 
         fixed = []
         for z in self.zeros:
             if self.name in self.zeros[z]:
-                #floatSomething(z)
                 self.fixPars(z)
                 self.switchOff(z)
             else:
                 if not z in fixed:
                     floatSomething(z)
                     fixed.append(z)
-
-        #remove redundant second components
-        self.fix2ndComponent("QCD")
-        self.workspace.var("f2_QCD").setVal(0.)
-        self.workspace.var("f2_QCD").setConstant(rt.kTRUE)
-                    
+        
+        #we take the 2nd component of the TTj from our control region
+        if self.name in ['Had','BJet','BJet5J']:
+            self.fix1stComponent("TTj")
+            self.workspace.var("f2_TTj").setVal(0.999)
+            self.workspace.var("f2_TTj").setConstant(rt.kTRUE)
                     
     def plot1DHistoAllComponents(self, inputFile, xvarname, nbins = 25, ranges=None, data = None):
         
@@ -74,24 +73,28 @@ class RazorMultiJetBox(RazorBox.RazorBox):
         if ranges is None:
             rangeNone = True
             ranges = ['']
-            
+        
+        factor = 50    
         #before I find a better way
         rangeCut = self.getVarRangeCutNamed(ranges=ranges)
         if data is None:
             data = RootTools.getDataSet(inputFile,'RMRTree', self.cut)
             data = data.reduce(rangeCut)
-        toyData = self.workspace.pdf(self.fitmodel).generate(self.workspace.set('variables'), 50*data.numEntries())
+        toyData = self.workspace.pdf(self.fitmodel).generate(self.workspace.set('variables'), factor*data.numEntries())
         toyData = toyData.reduce(self.getVarRangeCutNamed(ranges=ranges))
+        
+        #also show the data with loose leptons
+        dataLep = data.reduce('nLepton > 0')
 
         xmin = min([self.workspace.var(xvarname).getMin(r) for r in ranges])
         xmax = max([self.workspace.var(xvarname).getMax(r) for r in ranges])
 
         # define 1D histograms
-        histoData = rt.TH1D("histoData", "histoData",nbins, xmin, xmax)
-        histoToy = rt.TH1D("histoToy", "histoToy",nbins, xmin, xmax)
-        histoToyTTj = rt.TH1D("histoToyTTj", "histoToyTTj",nbins, xmin, xmax)
-        histoToyWln = rt.TH1D("histoToyWln", "histoToyWln",nbins, xmin, xmax)
-        histoToyZnn = rt.TH1D("histoToyZnn", "histoToyZnn",nbins, xmin, xmax)
+        histoData = self.setPoissonErrors(rt.TH1D("histoData", "histoData",nbins, xmin, xmax))
+        histoDataLep = self.setPoissonErrors(rt.TH1D("histoDataLep", "histoDataLep",nbins, xmin, xmax))
+        histoToy = self.setPoissonErrors(rt.TH1D("histoToy", "histoToy",nbins, xmin, xmax))
+        histoToyTTj = self.setPoissonErrors(rt.TH1D("histoToyTTj", "histoToyTTj",nbins, xmin, xmax))
+        histoToyQCD = self.setPoissonErrors(rt.TH1D("histoToyQCD", "histoToyQCD",nbins, xmin, xmax))
 
         def setName(h, name):
             h.SetName('%s_%s_%s_ALLCOMPONENTS' % (h.GetName(),name,'_'.join(ranges)) )
@@ -104,49 +107,92 @@ class RazorMultiJetBox(RazorBox.RazorBox):
         # project the data on the histograms
         #data.tree().Project("histoData",xvarname)
         data.fillHistogram(histoData,rt.RooArgList(self.workspace.var(xvarname)))
+        dataLep.fillHistogram(histoDataLep,rt.RooArgList(self.workspace.var(xvarname)))
         toyData.fillHistogram(histoToy,rt.RooArgList(self.workspace.var(xvarname)))
-
+        
+        #Cache the numbers
+        Ntt = self.workspace.var("Ntot_TTj").getVal()
+        Nqcd = self.workspace.var("Ntot_QCD").getVal()
+        
+        #Generate the TTj component    
+        self.workspace.var("Ntot_QCD").setVal(0.)
+        toyDataTTj = self.workspace.pdf(self.fitmodel).generate(self.workspace.set('variables'), int(factor*(data.numEntries()-Nqcd)))
+        toyDataTTj.fillHistogram(histoToyTTj,rt.RooArgList(self.workspace.var(xvarname)))
+        histoToyTTj.SetLineColor(rt.kRed)
+        histoToyTTj.SetLineWidth(2)
+        self.workspace.var("Ntot_QCD").setVal(Nqcd)
+        
+        #Generate the QCD component
+        self.workspace.var("Ntot_TTj").setVal(0.)
+        toyDataQCD = self.workspace.pdf(self.fitmodel).generate(self.workspace.set('variables'), int(factor*(data.numEntries()-Ntt)))
+        toyDataQCD.fillHistogram(histoToyQCD,rt.RooArgList(self.workspace.var(xvarname)))
+        histoToyQCD.SetLineColor(rt.kGreen)
+        histoToyQCD.SetLineWidth(2)
+        self.workspace.var("Ntot_TTj").setVal(Ntt)        
+        
         #put some protection in for divide by zero
         scaleFactor = 1.0
         if abs(histoToy.Integral()-0.0) > 1e-8:
             scaleFactor = histoData.Integral()/histoToy.Integral()
 
         histoToy.Scale(scaleFactor)
+        histoToyTTj.Scale(scaleFactor)
+        histoToyQCD.Scale(scaleFactor)
         SetErrors(histoToy, nbins)
+        SetErrors(histoToyTTj, nbins)
+        SetErrors(histoToyQCD, nbins)
         setName(histoData,xvarname)
+        setName(histoDataLep,xvarname)
         setName(histoToy,xvarname)
+        setName(histoToyTTj,xvarname)
+        setName(histoToyQCD,xvarname)
         histoData.SetMarkerStyle(20)
+        histoDataLep.SetLineWidth(2)
+        histoDataLep.SetLineStyle(rt.kDashed)
         histoToy.SetLineColor(rt.kBlue)
         histoToy.SetLineWidth(2)
 
         c = rt.TCanvas()
         c.SetName('DataMC_%s_%s_ALLCOMPONENTS' % (xvarname,'_'.join(ranges)) )
         histoData.Draw("pe")
+        
+        histoDataLep.Draw("histsame")
+
+        histoToyTTj.DrawCopy('histsame')
+        histoToyTTj.SetFillColor(rt.kRed)
+        histoToyTTj.SetFillStyle(3018)
+        histoToyTTj.Draw('e2same')  
+
+        histoToyQCD.DrawCopy('histsame')
+        histoToyQCD.SetFillColor(rt.kGreen)
+        histoToyQCD.SetFillStyle(3018)
+        histoToyQCD.Draw('e2same')  
 
         histoToy.DrawCopy('histsame')
         histoToy.SetFillColor(rt.kBlue)
         histoToy.SetFillStyle(3018)
         histoToy.Draw('e2same')
 
-        #histoData.Draw("pesame")
-
-        #leg = rt.TLegend(0.6,0.6,0.9,0.9)
-        #leg.SetFillColor(0)
-        #leg.AddEntry(histoToyWln.GetName(),"W+jets","l")
-        #leg.AddEntry(histoToyTTj.GetName(),"t#bar{t}","l")
-        #leg.AddEntry(histoToy.GetName(),"Total","l")
-        #leg.Draw()
-
-        histToReturn = [histoToy, histoData, c]
+        histToReturn = [histoToy, histoToyQCD, histoToyTTj, histoData, histoDataLep, c]
 
         return histToReturn
 
+#    # to be removed eventually
+#    def plot(self, inputFile, store, box):
+#        store.store(self.plot2D(inputFile, "MR", "Rsq", ranges=['fR1,fR2,fR3,fR4,fR5']), dir=box)
+#        [store.store(s, dir=box) for s in self.plot1DHistoAllComponents(inputFile, "MR", 80, ranges=['fR1,fR2,fR3,fR4,fR5'])]
+#        [store.store(s, dir=box) for s in self.plot1DHistoAllComponents(inputFile, "Rsq", 80, ranges=['fR1,fR2,fR3,fR4,fR5'])]
+#        for r in ['FULL','fR1','fR2','fR3','fR4','fR5']:
+#            store.store(self.plot2D(inputFile, "MR", "Rsq", ranges=[r]), dir=box)
+#            [store.store(s, dir=box) for s in self.plot1DHistoAllComponents(inputFile, "MR", 80, ranges=[r])]
+#            [store.store(s, dir=box) for s in self.plot1DHistoAllComponents(inputFile, "Rsq", 80, ranges=[r])]
+
     # to be removed eventually
     def plot(self, inputFile, store, box):
-        store.store(self.plot2D(inputFile, "MR", "Rsq", ranges=['fR1,fR2,fR3,fR4,fR5']), dir=box)
-        [store.store(s, dir=box) for s in self.plot1DHistoAllComponents(inputFile, "MR", 80, ranges=['fR1,fR2,fR3,fR4,fR5'])]
-        [store.store(s, dir=box) for s in self.plot1DHistoAllComponents(inputFile, "Rsq", 80, ranges=['fR1,fR2,fR3,fR4,fR5'])]
-        for r in ['FULL','fR1','fR2','fR3','fR4','fR5']:
-            store.store(self.plot2D(inputFile, "MR", "Rsq", ranges=[r]), dir=box)
-            [store.store(s, dir=box) for s in self.plot1DHistoAllComponents(inputFile, "MR", 80, ranges=[r])]
-            [store.store(s, dir=box) for s in self.plot1DHistoAllComponents(inputFile, "Rsq", 80, ranges=[r])]
+        store.store(self.plot2D(inputFile, "MR", "Rsq", ranges=['FULL']), dir=box)
+        [store.store(s, dir=box) for s in self.plot1DHistoAllComponents(inputFile, "MR", 80, ranges=['FULL'])]
+        [store.store(s, dir=box) for s in self.plot1DHistoAllComponents(inputFile, "Rsq", 80, ranges=['FULL'])]
+#        for r in ['FULL']:
+#            store.store(self.plot2D(inputFile, "MR", "Rsq", ranges=[r]), dir=box)
+#            [store.store(s, dir=box) for s in self.plot1DHistoAllComponents(inputFile, "MR", 80, ranges=[r])]
+#            [store.store(s, dir=box) for s in self.plot1DHistoAllComponents(inputFile, "Rsq", 80, ranges=[r])]
