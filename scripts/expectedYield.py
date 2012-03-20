84c9eeb77a2842fbb42902eaca132f31079c1a4a
wreece <>
2012-03-20 13:10:11 +0000

    adds a config option for the binning

--- scripts/expectedYield.py
+++ scripts/expectedYield.py
@@ -1,15 +1,53 @@
 from optparse import OptionParser
 import ROOT as rt
 import sys
+from RazorCombinedFit.Framework import Config
+
+def defineParser():
+    
+    parser = OptionParser()
+    parser.add_option('-c','--config',dest="config",type="string",default=None,
+                  help="Name of the config file to use")
+    parser.add_option('--box',dest="box",type="string",default='Had',
+                  help="Name of the box to use when reading the config")
+    parser.add_option('--scale-lumi',dest="scale_lumi",type="int", default=1,
+                  help="The lumi scale factor used when genrating the toys")
+    parser.add_option('-o','--output',dest="output",type="string", default='expected_Had.root',
+                  help="Name of the root file to store everything in")
+    parser.add_option('--options',dest="options", default=False,action='store_true',
+                  help="Use the options parser output")
+    return parser
 
 if __name__ == '__main__':
 
+    parser = defineParser()
+    (options,args) = parser.parse_args()
+    
+    if options.options:
+        ScaleFactor = options.scale_lumi
+        Box = options.box
+        label = options.output
+        inputFiles = args
+        
+        cfg = Config.Config(options.config)
+        
+        binning = cfg.getBinning(Box)
+        MRbins = binning[0]
+        Rsqbins = binning[1]
+        
+    else:
+        
+        ScaleFactor = int(sys.argv[1])
+        label = sys.argv[2]
+        Box = sys.argv[3]
+        inputFiles = sys.argv[4:]
+        
+        # bins in mR
+        MRbins = [300, 350, 400, 450, 650, 800, 1000, 1250, 1500, 7000]
+        # bins in R^2
+        Rsqbins = [0.09, 0.16, 0.20, 0.30, 0.40, 0.45, 0.50]
+    
     myTree = rt.TTree("myTree", "myTree")
-
-    # bins in mR
-    MRbins = [300, 350, 400, 450, 650, 800, 1000, 1250, 1500, 7000]
-    # bins in R^2
-    Rsqbins = [0.09, 0.16, 0.20, 0.30, 0.40, 0.45, 0.50]
 
     # create magic string
     magicString= "struct MyStruct{"
@@ -23,11 +61,6 @@ if __name__ == '__main__':
     rt.gROOT.ProcessLine(magicString)
     from ROOT import MyStruct
 
-    # read inputs
-    ScaleFactor = int(sys.argv[1])
-    label = sys.argv[2]
-    Box = sys.argv[3]
-
     # associate the bins to the corresponding variables in the structure
     s = MyStruct()
     for ix in range(0, len(MRbins)-1):
@@ -38,10 +71,9 @@ if __name__ == '__main__':
             myTree.Branch(myBin[0], rt.AddressOf(s,varName),'%s/D' %varName)
     
     treeName = "RMRTree"
-    for i in range(4,len(sys.argv)):
-        print "processing file: ", sys.argv[i]
-        myfile = rt.TFile(sys.argv[i])
-        #myfile = rt.TFile("%s_%i.root" %(sys.argv[4],i))
+    for i in xrange(len(inputFiles)):
+        print "processing file: ", inputFiles[i]
+        myfile = rt.TFile.Open(inputFiles[i])
         gdata = myfile.Get(treeName)
         if gdata == None: continue
         if gdata.InheritsFrom("RooDataSet") != True: continue
