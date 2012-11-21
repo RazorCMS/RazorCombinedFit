af5befee818e25b225acb38a5050594fd3135ad6
woodson <>
2012-11-21 11:11:36 +0000

    new chain for making unweighted cocktails

--- scripts/CocktailDataset2UnweightedToy.py
+++ scripts/CocktailDataset2UnweightedToy.py
@@ -1,3 +1,4 @@
+#! /usr/bin/env python
 from optparse import OptionParser
 
 import ROOT as rt
@@ -5,41 +6,75 @@ import RootTools
 from RazorCombinedFit.Framework import Config
 import os.path
 
-class Marker:
-    pass 
-
-def writeCocktail(box, files, outdir):
-    """Write a cocktail file using the weights from the datasets"""
+def getUnweightedToy(box, files, outdir):
+    """Get the cocktail dataset from the file"""
     
     components = [os.path.basename(f).split('_')[0] for f in files]
     if len(components) != len(set(components)):
         raise Exception('Some components not unique for box %s: %s' % (box,components))
-    
-    ds = []
-    for f in files:
-        ds.append(RootTools.getDataSet(f,'RMRTree'))
-    if not ds:
-        raise Exception('Not enough datasets found: %i' % len(ds))
 
-    row = ds[0].get()
+    f = files[0]
+    wdata = RootTools.getDataSet(f,'RMRTree')
+    if not wdata:
+        raise Exception('No dataset found')
+        
+    row = wdata.get()
     row.Print("V")
+
+    MR = row['MR']
+    Rsq = row['Rsq']
+    nBtag = row['nBtag']
+    CHARGE = row['CHARGE']
+    
+    varSet = rt.RooArgSet(MR,Rsq,nBtag,CHARGE)
+    varList = rt.RooArgList(MR,Rsq,nBtag)
+    uwdata = rt.RooDataSet('RMRTree','Unweighted Cocktail',varSet)
+    
+
     
     mRmin = row['MR'].getMin()
+    mRmax = row['MR'].getMax()
     rsqMin = row['Rsq'].getMin()
+    rsqMax = row['Rsq'].getMax()
+    nbtagMin = row['nBtag'].getMin()
+    nbtagMax = row['nBtag'].getMax()
+    
     rMin = rt.TMath.Sqrt(rsqMin)
     
-    tData = rt.RooDataSet('RMRTree','Total Dataset',row)
-    for d in ds:
-        tData.append(d)
-    wData = rt.RooDataSet('RMRTree','Weighted Cocktail',tData,row,'MR>0','W')
+    myTH3 = rt.TH3D("h%s"%box, "h%s"%box, 100, mRmin, mRmax, 100, rsqMin, rsqMax, int(nbtagMax-nbtagMin), nbtagMin, nbtagMax)
+    #myTH2 = rt.TH2D("h", "h", 100, mRmin, mRmax, 100, rsqMin, rsqMax)
 
-    output = rt.TFile.Open(outdir+"/SMCocktail_MR"+str(mRmin)+"_R"+str(rMin)+"_"+box+'.root','RECREATE')
+    # fills automatically with weight
+    wdata.fillHistogram(myTH3, varList,"MR>0")
+    
+    
+    print wdata.weight()
+    Nev = myTH3.Integral()
+    Nent = myTH3.GetEntries()
+    print "weighted events %.1f"% Nev
+    print "entries  %d"% Nent
+    Npois = rt.RooRandom.randomGenerator().Poisson(Nev)
+    for i in range(0,int(Nev)):
+        myMR = rt.Double()
+        myRsq = rt.Double()
+        mynBtag = rt.Double()
+        myTH3.GetRandom3(myMR,myRsq,mynBtag)
+        mynBtag = int(mynBtag)
+        varSet.setRealValue('MR',myMR)
+        varSet.setRealValue('Rsq',myRsq)
+        varSet.setRealValue('nBtag',mynBtag)
+        varSet.setRealValue('CHARGE',1.0)
+        uwdata.add(varSet)
+   
+    output = rt.TFile.Open(outdir+"/SMCocktail_GENTOY_MR"+str(mRmin)+"_R"+str(rMin)+"_"+box+'.root','RECREATE')
     print 'Writing',output.GetName()
-    wData.Write()
+    uwdata.Write()
+    myTH3.Write()
     output.Close()
    
+   
 if __name__ == '__main__':
-    
+
     parser = OptionParser()
     parser.add_option('-c','--config',dest="config",type="string",default=None,
                   help="Name of the config file to use")
@@ -56,7 +91,6 @@ if __name__ == '__main__':
         topDir = os.path.abspath(os.path.dirname(inspect.getsourcefile(writeCocktail)))
         options.config = os.path.join(topDir,'boxConfig.cfg')    
     cfg = Config.Config(options.config)
-    
     print 'Input files are %s' % ', '.join(args)
 
     boxes = {}
@@ -74,4 +108,4 @@ if __name__ == '__main__':
             "File '%s' of unknown type. Looking for .root files only" % f
 
     for box, files in boxes.iteritems():
-        writeCocktail(box, files, options.outdir)
+        getUnweightedToy(box, files, options.outdir)
