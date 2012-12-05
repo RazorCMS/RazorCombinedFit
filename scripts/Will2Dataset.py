705f1038819a2570ff8d8070c62bec8efb72c427
wreece <>
2012-12-05 13:19:55 +0000

    draft version - do not use

--- scripts/Will2Dataset.py
+++ scripts/Will2Dataset.py
@@ -4,6 +4,7 @@ import os, array
 import ROOT as rt
 import RootTools
 from RazorCombinedFit.Framework import Config
+#import CalcBDT
 
 boxMap = {'MuEle':0,'MuMu':1,'EleEle':2,'Mu':3,'Ele':4,'Had':5,'BJet':6}
 cross_sections = {'SingleTop_s':4.21,'SingleTop_t':64.6,'SingleTop_tw':10.6,\
@@ -31,10 +32,14 @@ class HadDumper(object):
 
         self.vars['mr_var'] = array.array('f',[0])
         self.vars['rsq_var'] = array.array('f',[0])
+        self.vars['nvertex_var'] = array.array('f',[0])
         
         self.reader.AddSpectator('MR',self.vars['mr_var'])
         self.reader.AddSpectator('RSQ',self.vars['rsq_var'])
+        self.reader.AddSpectator('nVertex',self.vars['nvertex_var'])
         self.reader.BookMVA('BDT','/afs/cern.ch/user/w/wreece/work/CMGTools/V5_6_0/CMGTools/CMSSW_5_3_3_patch3/src/CMGTools/Susy/prod/MultiJet/TMVAClassification_BDT.weights.xml')        
+        
+        self.jets = []
 
     def thetaH1(self):
         if self.tree.bestHemi == 1:
@@ -63,8 +68,13 @@ class HadDumper(object):
             return self.tree.hemi1WMass
         return self.tree.hemi2WMass
 
+    def tag_jets(self):
+        #order by btag output - high to low
+        jets = sorted([(self.tree.jet_csv.at(i),i) for i in xrange(len(self.tree.jet_csv))],reverse=True)
+        self.jets = [i for b, i in jets]
+
     def jetNpt(self, n):
-        return self.tree.jet_pt.at(n)
+        return self.tree.jet_pt.at(self.jets[n])
 
     def jet1pt(self):
         return self.jetNpt(0)
@@ -80,37 +90,39 @@ class HadDumper(object):
         return self.jetNpt(5)
 
     def jet1mult(self):
-        return self.tree.jet_mult.at(0)
+        return self.tree.jet_mult.at(self.jets[0])
     def jet2mult(self):
-        return self.tree.jet_mult.at(1)
+        return self.tree.jet_mult.at(self.jets[1])
     def jet3mult(self):
-        return self.tree.jet_mult.at(2)
+        return self.tree.jet_mult.at(self.jets[2])
     def jet4mult(self):
-        return self.tree.jet_mult.at(3)
+        return self.tree.jet_mult.at(self.jets[3])
     def jet5mult(self):
-        return self.tree.jet_mult.at(4)
+        return self.tree.jet_mult.at(self.jets[4])
     def jet6mult(self):
-        return self.tree.jet_mult.at(5)
+        return self.tree.jet_mult.at(self.jets[5])
 
     def jet1girth(self):
-        return self.tree.jet_girth.at(0)
+        return self.tree.jet_girth_ch.at(self.jets[0])
     def jet2girth(self):
-        return self.tree.jet_girth.at(1)
+        return self.tree.jet_girth_ch.at(self.jets[1])
     def jet3girth(self):
-        return self.tree.jet_girth.at(2)
+        return self.tree.jet_girth_ch.at(self.jets[2])
     def jet4girth(self):
-        return self.tree.jet_girth.at(3)
+        return self.tree.jet_girth_ch.at(self.jets[3])
     def jet5girth(self):
-        return self.tree.jet_girth.at(4)
+        return self.tree.jet_girth_ch.at(self.jets[4])
     def jet6girth(self):
-        return self.tree.jet_girth.at(5)
+        return self.tree.jet_girth_ch.at(self.jets[5])
+
     
     def bdt(self):
-        
+        self.tag_jets()
         for h in self.headers_for_MVA():
             self.vars['%s_var'%h][0] = getattr(self,h)()
         self.vars['mr_var'] = self.tree.MR
         self.vars['rsq_var'] = self.tree.RSQ
+        self.vars['nvertex_var'] = self.tree.nVertex
         return self.reader.EvaluateMVA('BDT')
 
 
@@ -120,11 +132,11 @@ class HadBox(object):
         self.name = 'Had'
         self.dumper = dumper
     def __call__(self, tree):
-        nTight = tree.nMuonTight + tree.nElectronTight + tree.nTauTight
-        nLoose = tree.nMuonLoose + tree.nElectronLoose + tree.nTauLoose
+        nTight = tree.nMuonTight + tree.nElectronTight
+        nLoose = tree.nMuonLoose + tree.nElectronLoose
         return tree.metFilter and not tree.muTriggerFilter and not tree.eleTriggerFilter and\
             tree.MR >= 450 and tree.RSQ >= 0.03 and tree.hadBoxFilter and tree.hadTriggerFilter and\
-            tree.nJet >= 6 and tree.nCSVL == 0 and nTight == 0 and nLoose == 0
+            tree.nJet >= 6 and tree.nCSVL == 0 and nTight == 0 and nLoose == 0 and not tree.isolatedTrack10Filter
     
 class BJetBox(object):
     """The BJet search box used in the analysis"""
@@ -132,11 +144,11 @@ class BJetBox(object):
         self.name = 'BJet'
         self.dumper = dumper
     def __call__(self, tree):
-        nTight = tree.nMuonTight + tree.nElectronTight + tree.nTauTight
-        nLoose = tree.nMuonLoose + tree.nElectronLoose + tree.nTauLoose
+        nTight = tree.nMuonTight + tree.nElectronTight
+        nLoose = tree.nMuonLoose + tree.nElectronLoose
         return tree.metFilter and not tree.muTriggerFilter and not tree.eleTriggerFilter and\
              tree.MR >= 450 and tree.RSQ >= 0.03 and tree.hadBoxFilter and tree.hadTriggerFilter\
-              and tree.nJet >= 6 and tree.nCSVM > 0 and nTight == 0 and nLoose == 0        
+              and tree.nJet >= 6 and tree.nCSVM > 0 and nTight == 0 and nLoose == 0 and not tree.isolatedTrack10Filter 
 
 class CR6JSingleLeptonBVetoBox(object):
     """A control region for the Had box: One lepton, no bjets - Should give handle on W+Jets"""
@@ -145,7 +157,7 @@ class CR6JSingleLeptonBVetoBox(object):
         self.dumper = None
     def __call__(self, tree):
         nTight = tree.nMuonTight + tree.nElectronTight
-        nLoose = tree.nMuonLoose + tree.nElectronLoose + tree.nTauLoose
+        nLoose = tree.nMuonLoose + tree.nElectronLoose
         return tree.metFilter and\
              tree.MR >= 450 and tree.RSQ >= 0.03 and tree.hadBoxFilter and tree.hadTriggerFilter\
              and tree.nJet >= 6 and tree.nCSVL == 0 and nTight == 1 and nLoose == 1        
@@ -177,7 +189,12 @@ def convertTree2Dataset(tree, outputFile, config, min, max, filter, run, write =
     workspace.factory('nJet[0,0,15.0]')
     workspace.factory('W[0,0,+INF]')
     workspace.factory('BDT[0,-INF,+INF]')
-
+    workspace.factory('genInfo[0,-INF,+INF]')
+    
+    if filter.dumper is not None:
+        for h in filter.dumper.headers_for_MVA():
+            workspace.factory('%s[0,-INF,+INF]' % h)
+    
     args = workspace.allVars()
     data = rt.RooDataSet('RMRTree','Selected R and MR',args)
     
@@ -224,7 +241,7 @@ def convertTree2Dataset(tree, outputFile, config, min, max, filter, run, write =
         except AttributeError:
             pass
         
-        nBtag = tree.NBJET
+        nBtag = tree.nCSVM
 
         #set the RooArgSet and save
         a = rt.RooArgSet(args)
@@ -244,6 +261,12 @@ def convertTree2Dataset(tree, outputFile, config, min, max, filter, run, write =
         a.setRealValue('nVertex',tree.nVertex)        
         a.setRealValue('W',1.0)
         a.setRealValue('BDT',bdt)
+        
+        a.setRealValue('genInfo',tree.genInfo)
+        
+        if filter.dumper is not None:
+            for h in filter.dumper.headers_for_MVA():
+                a.setRealValue(h,getattr(filter.dumper,h)())
         
         data.add(a)
     numEntries = data.numEntries()
@@ -276,6 +299,8 @@ if __name__ == '__main__':
                   help="Output directory to store datasets")
     parser.add_option('-x','--box',dest="box",default=None,type="string",
                   help="Specify only one box")
+    parser.add_option('--name',dest="name",default='RMRTree',type="string",
+                  help="The name of the TTree to use")
       
     (options,args) = parser.parse_args()
     
@@ -287,7 +312,7 @@ if __name__ == '__main__':
     
     print 'Input files are %s' % ', '.join(args)
     
-    chain = rt.TChain('RMRTree')
+    chain = rt.TChain(options.name)
     fName = None
     for f in args:
         if f.lower().endswith('.root'):
