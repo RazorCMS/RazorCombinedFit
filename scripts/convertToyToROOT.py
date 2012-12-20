29fade883fb6902e0a7c678e61e99ce42f3be8bf
salvati <>
2012-12-20 15:37:42 +0000

    2012 multijet script

--- scripts/convertToyToROOT.py
+++ scripts/convertToyToROOT.py
@@ -4,16 +4,15 @@ import sys
     
 if __name__ == '__main__':
     
-    MR = rt.RooRealVar("MR", "MR", 350., 2500.)
-    Rsq = rt.RooRealVar("Rsq", "Rsq", 0.11, 1.5)
-    nBtag = rt.RooRealVar("nBtag", "nBtag", 1.0, 5.0)
+    MR = rt.RooRealVar("MR", "MR", 450., 4000.)
+    Rsq = rt.RooRealVar("Rsq", "Rsq", 0.03, 1.)
     dir = sys.argv[1] 
     # to add the category
     for i in range(0,2000):
         rt.gROOT.ProcessLine("delete gDirectory->FindObject(\"RMRTree\");")
         tree = rt.TTree("RMRTree","RMRTree")
         filename = sys.argv[1]+"_"+str(i)+".txt"
-        tree.ReadFile(filename,"MR/D:Rsq/D:nBtag/D")
+        tree.ReadFile(filename,"MR/D:Rsq/D")
         if tree.GetEntries() <1: continue
         myfile = rt.TFile.Open("%s_%i.root" %(dir, i), "recreate")
         tree.Write()
