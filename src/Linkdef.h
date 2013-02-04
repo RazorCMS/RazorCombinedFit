d55de9f89f96a5433e2398313802bf4f92048839
ssekmen <>
2013-02-04 10:12:28 +0000

    add PDF with HLT BNN

--- src/Linkdef.h
+++ src/Linkdef.h
@@ -17,6 +17,7 @@
 #pragma link C++ defined_in "RooRazor3DSignal.h";
 #pragma link C++ defined_in "RooRazor2DTail.h";
 #pragma link C++ defined_in "RooRazor2DTail_SYS.h";
+#pragma link C++ defined_in "RooRazor2DTailBNN_SYS.h";
 #pragma link C++ defined_in "RooRazor2DTurnOn.h";
 #pragma link C++ defined_in "RooRazorLShape.h";
 #pragma link C++ defined_in "RooTwoBin.h";
