8cebbb2458ccc4870deff72eb9a093e910ff4ea9
lucieg <>
2013-03-12 09:04:14 +0000

    upadted mergeFitResults for mu and ele files

--- scripts/mergeFitResults.py
+++ scripts/mergeFitResults.py
@@ -13,11 +13,15 @@ if __name__ == '__main__':
                   help="Name of the output file to use",metavar='FILE')
     parser.add_option('--bjet',dest="bjet",type="string",default=None,
                   help="Name of the input file for the BJet box",metavar='FILE')    
+    parser.add_option('--ele',dest="ele",type="string",default=None,
+                  help="Name of the input file for the Ele box",metavar='FILE')    
+    parser.add_option('--mu',dest="mu",type="string",default=None,
+                  help="Name of the input file for the Mu box",metavar='FILE')    
     parser.add_option('--had',dest="had",type="string",default=None,
                   help="Name of the input file for the Had box",metavar='FILE')      
     (options,args) = parser.parse_args()
-    if options.bjet is None or options.had is None:
-        parser.error("Need to specify both the --bjet and --had options")
+   # if options.bjet is None or options.had is None:
+   #     parser.error("Need to specify both the --bjet and --had options")
 
     def getBoxContents(box, store):
         rt.gDirectory.cd(box)
@@ -31,10 +35,23 @@ if __name__ == '__main__':
     
     store = RootTools.RootFile.RootFile(options.output)
 
-    hadFile = rt.TFile.Open(options.had)
-    getBoxContents('Had',store)
-    
-    bjetFile = rt.TFile.Open(options.bjet)
-    getBoxContents('BJet',store)
+    if not(options.had is None) :
+        hadFile = rt.TFile.Open(options.had)
+        getBoxContents('BJetLS',store)
+        hadFile = rt.TFile.Open(options.had)
+        getBoxContents('BJetHS',store)
+
+    if not(options.bjet is None) :
+        bjetFile = rt.TFile.Open(options.bjet)
+        getBoxContents('BJet',store)
+
+    if not(options.ele is None) :
+        eleFile = rt.TFile.Open(options.ele)
+        getBoxContents('Ele',store)
+
+    if not(options.mu is None) :
+        muFile = rt.TFile.Open(options.mu)
+        getBoxContents('Mu',store)
+
 
     store.write()
