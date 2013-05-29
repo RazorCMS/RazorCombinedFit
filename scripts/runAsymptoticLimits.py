9bf73c7cc05359ff2a326b51f1571b3145375b1f
salvati <>
2013-05-29 17:09:59 +0000

    Modified xsecrange for mLSP50

--- scripts/runAsymptoticLimits.py
+++ scripts/runAsymptoticLimits.py
@@ -5,36 +5,93 @@ import time, datetime
 
 def getXsecRange(box,gluinopoint):
 
+    # if gluinopoint == 150 :
+    #     return [0.01, 0.05, 0.07]
+    # elif gluinopoint ==175  :
+    #     return [0.01, 0.05, 0.07]
+    # elif gluinopoint == 200 :
+    #     return [0.01, 0.05, 0.07]
+    # elif gluinopoint == 225 :
+    #     return [0.01, 0.05, 0.1, 0.2]
+    # elif gluinopoint < 300 :
+    #     return [0.01, 0.05, 0.1, 0.5]
+    # elif gluinopoint <= 300 :
+    #     return [0.01, 0.05, 0.1, 0.5]
+    # elif gluinopoint < 400 :
+    #     return [0.01, 0.05, 0.1, 0.5]
+    # elif gluinopoint == 400 :
+    #     return [ 0.1, 0.5, 1.0]
+    # elif gluinopoint <= 500 :
+    #     return [0.01, 0.05, 0.1, 0.2, 0.3]
+    # elif gluinopoint == 525 :
+    #     return [0.01, 0.05, 0.1, 0.2]
+    # elif gluinopoint == 550 :
+    #     return [0.01, 0.05, 0.1, 0.2, 0.3]
+    # elif gluinopoint == 575 :
+    #     return [0.01, 0.05, 0.1, 0.2, 0.3]
+    # elif gluinopoint == 600 :
+    #     return [0.01, 0.05, 0.1, 0.2]
+    # elif gluinopoint == 625 :
+    #     return [ 0.01, 0.05, 0.1, 0.2]
+    # elif gluinopoint > 625 :
+    #     return [  0.01, 0.05, 0.1, 0.2]
+
     if gluinopoint == 150 :
-        return [0.01, 0.05, 0.07]
+      return [0.01, 0.05, 0.07]
     elif gluinopoint ==175  :
-        return [0.01, 0.05, 0.07]
+      return [0.01, 0.05, 0.07]
     elif gluinopoint == 200 :
-        return [0.01, 0.05, 0.07]
+          return [0.01, 0.05, 0.07, 0.5, 0.7, 1.0]
     elif gluinopoint == 225 :
-        return [0.01, 0.05, 0.1, 0.2]
-    elif gluinopoint < 300 :
-        return [0.01, 0.05, 0.1, 0.5]
-    elif gluinopoint <= 300 :
-        return [0.01, 0.05, 0.1, 0.5]
-    elif gluinopoint < 400 :
-        return [0.01, 0.05, 0.1, 0.5]
+        return [ 0.05, 0.1, 0.2, 0.5, 0.7, 1.0]
+    elif gluinopoint == 250 :
+        return [ 0.05, 0.1, 0.5, 0.7, 1.0]
+    elif gluinopoint == 275 :
+        return [ 1.0 ]#0.05, 0.1, 0.5, 0.7, 1.0]
+    elif gluinopoint == 300 :
+        return [ 0.5, 0.7]# 0.05, 0.1, 0.5, 0.7]
+    elif gluinopoint == 325 :
+        return [  0.1, 0.5, 0.7, 1.0]
+    elif gluinopoint == 350 :
+        return [  0.1, 0.5, 0.7, 1.0]
+    elif gluinopoint == 375 :
+        return [  0.1, 0.5, 0.7, 1.0]
     elif gluinopoint == 400 :
-        return [ 0.1, 0.5, 1.0]
-    elif gluinopoint <= 500 :
-        return [0.01, 0.05, 0.1, 0.2, 0.3]
+        return [ 0.1, 0.5, 0.7, 1.0]
+    elif gluinopoint == 425 :
+          return [ 0.05, 0.1, 0.2, 0.3, 0.5, 1.0]
+    elif gluinopoint == 450 :
+        return [ 0.05, 0.1, 0.2, 0.3, 0.5, 1.0]
+    elif gluinopoint == 475 :
+        return [ 0.05, 0.1, 0.2, 0.3, 0.5]
+    elif gluinopoint == 500 :
+        return [ 0.05, 0.1, 0.2, 0.3]
     elif gluinopoint == 525 :
         return [0.01, 0.05, 0.1, 0.2]
     elif gluinopoint == 550 :
-        return [0.01, 0.05, 0.1, 0.2, 0.3]
+        return [ 0.05, 0.1, 0.2, 0.3]
     elif gluinopoint == 575 :
-        return [0.01, 0.05, 0.1, 0.2, 0.3]
+        return [ 0.05, 0.1, 0.2, 0.3]
     elif gluinopoint == 600 :
-        return [0.01, 0.05, 0.1, 0.2]
+        return [ 0.01, 0.05, 0.1, 0.2]
     elif gluinopoint == 625 :
         return [ 0.01, 0.05, 0.1, 0.2]
-    elif gluinopoint > 625 :
+    elif gluinopoint == 625 :
         return [  0.01, 0.05, 0.1, 0.2]
+    elif gluinopoint == 650 :
+        return [ 0.01, 0.05, 0.1]
+    elif gluinopoint == 675 :
+        return [ 0.01, 0.05, 0.1]
+    elif gluinopoint == 700 :
+        return [ 0.01, 0.03, 0.05, 0.1] 
+    elif gluinopoint == 725 :
+        return [ 0.01, 0.03, 0.05, 0.1]
+    elif gluinopoint == 750 :
+        return [ 0.01, 0.03, 0.05, 0.1]
+    elif gluinopoint == 775 :
+        return [ 0.01, 0.03, 0.05, 0.1]
+    elif gluinopoint == 800 :
+        return [ 0.01, 0.03, 0.05, 0.1]
 
 
     
