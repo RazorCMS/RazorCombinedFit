6e04019a6e2fae42d55d06a870cf4dbe3b279a43
Ann <sezata@gmail.com>
2013-07-18 22:25:44 +0200

    compilation additions

--- src/Linkdef.h
+++ src/Linkdef.h
@@ -15,6 +15,7 @@
 #pragma link C++ defined_in "RooTwoSideGaussianWithTwoExponentialTails.h";
 #pragma link C++ defined_in "RooTwoSideGaussianWithThreeExponentialTails.h";
 #pragma link C++ defined_in "RooTwoSideGaussianWithOneExponentialTailAndOneInverseN.h";
+#pragma link C++ defined_in "RooTwoSideGaussianWithOneExponentialTailAndOneXDependentExponential.h"; 
 #pragma link C++ defined_in "RooRazor2DSignal.h";
 #pragma link C++ defined_in "RooRazor3DSignal.h";
 #pragma link C++ defined_in "RooRazor2DTail.h";
