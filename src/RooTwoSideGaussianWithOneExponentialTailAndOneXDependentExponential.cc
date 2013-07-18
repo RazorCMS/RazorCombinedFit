c8b09b358a5bf2aba58bc7aeb4d62ddc2c0dcd83
Ann <sezata@gmail.com>
2013-07-18 22:24:47 +0200

    added new analysis with x-dependent exponential term

---
+++ src/RooTwoSideGaussianWithOneExponentialTailAndOneXDependentExponential.cc
@@ -0,0 +1,91 @@
+//---------------------------------------------------------------------------
+#include "RooFit.h"
+
+#include "Riostream.h"
+#include <math.h>
+
+#include "RooTwoSideGaussianWithOneExponentialTailAndOneXDependentExponential.h"
+#include "RooRealVar.h"
+
+ClassImp(RooTwoSideGaussianWithOneExponentialTailAndOneXDependentExponential)
+//---------------------------------------------------------------------------
+RooTwoSideGaussianWithOneExponentialTailAndOneXDependentExponential::RooTwoSideGaussianWithOneExponentialTailAndOneXDependentExponential(const char *name,
+   const char *title,
+   RooAbsReal &_x, RooAbsReal &_x0,
+   RooAbsReal &_sigma_l, RooAbsReal &_sigma_r, RooAbsReal &_s1, RooAbsReal &_s2, RooAbsReal &_a1, 
+   RooAbsReal &_f1) :
+   RooAbsPdf(name, title), 
+   X("X", "Dependent", this, _x),
+   X0("X0", "Gaussian Mean", this, _x0),
+   SigmaL("SigmaL", "Left sigma", this, _sigma_l),
+   SigmaR("SigmaR", "Right sigma", this, _sigma_r),
+   S1("S1", "Exponent of the first tail term", this, _s1),
+   S2("S2", "Exponent of the second tail term", this, _s2),
+   A1("A1", "Denominator of the second tail term", this, _a1),
+   F1("F1", "Fraction", this, _f1)
+{
+}
+//---------------------------------------------------------------------------
+RooTwoSideGaussianWithOneExponentialTailAndOneXDependentExponential::RooTwoSideGaussianWithOneExponentialTailAndOneXDependentExponential(const RooTwoSideGaussianWithOneExponentialTailAndOneXDependentExponential& other, const char* name) :
+   RooAbsPdf(other, name), X("X", this, other.X), X0("X0", this, other.X0),
+   SigmaL("SigmaL", this, other.SigmaL), SigmaR("SigmaR", this, other.SigmaR),
+   S1("S1", this, other.S1), S2("S2", this, other.S2), A1("A1", this, other.A1), 
+   F1("F1", this, other.F1)
+{
+}
+//---------------------------------------------------------------------------
+Double_t RooTwoSideGaussianWithOneExponentialTailAndOneXDependentExponential::evaluate() const
+{
+
+  double value = 0.;
+  double XC = (S1 + F1 * S2) / (1 + F1) * SigmaR * SigmaR + X0;
+
+  if (X < X0)   // Left-hand side, normal gaussian
+    value = exp(-((X - X0) * (X - X0)) / (2 * SigmaL * SigmaL));
+  else if ((X >= X0 && X < XC) || (S1 < 0) || (S2 < 0))   // if S1 or S2 < 0, normal two-sided gaussian, no tail
+    value = exp(-((X - X0) * (X - X0)) / (2 * SigmaR * SigmaR));
+  else   // fun: X > XC > X0
+    {
+      double A = exp(-((XC - X0) * (XC - X0)) / (2 * SigmaR * SigmaR)) / (1 + F1);
+      value = A * (exp(-S1 * (X - XC)) + F1 * exp(-S2 * ((X - XC) / (1.0 + A1 * (X - XC)))));
+    }
+
+   return value;
+}
+//---------------------------------------------------------------------------
+Int_t RooTwoSideGaussianWithOneExponentialTailAndOneXDependentExponential::getAnalyticalIntegral(RooArgSet& allVars, RooArgSet& analVars, const char* rangeName) const 
+{
+  return 0;
+  // integral on X
+  if (matchArgs(allVars, analVars, X)) return 1;
+  return 0;
+}
+//---------------------------------------------------------------------------
+Double_t RooTwoSideGaussianWithOneExponentialTailAndOneXDependentExponential::analyticalIntegral(Int_t code, const char* rangeName) const
+{
+   const Double_t xmin = X.min(rangeName); const Double_t xmax = X.max(rangeName);
+   double XC = S1 * SigmaR * SigmaR + X0;
+
+   double integral = 0.;
+   double xlow1 = 0.;
+   double xhigh1 = 0.;
+   double xlow2 = 0.;
+   double xhigh2 = 0.;
+   double xlow3 = 0.;
+   double xhigh3 = 0.;
+
+   if(code==1) { 
+     if(xmin < X0 && xmax <=X0)
+       {
+	 xlow1 = xmin;
+	 xhigh1 = xmax;
+       }
+
+     //integral += ROOT::Math::normal_cdf(SigmaL,xhigh1,X0) - ROOT::Math::normal_cdf(SigmaL,xlow1,X0);
+     //integral += ROOT::Math::normal_cdf(SigmaR,xhigh2,X0) - ROOT::Math::normal_cdf(SigmaR,xlow2,X0);
+
+   }
+   return integral;
+   
+}
+//---------------------------------------------------------------------------
