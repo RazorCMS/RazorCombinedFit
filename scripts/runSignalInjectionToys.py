3a294f490fc97a957a654dec019f31a4d62006e9
Javier <jduarte@caltech.edu>
2013-07-21 23:20:11 -0700

    new mc closure for avi

--- scripts/runSignalInjectionToys.py
+++ scripts/runSignalInjectionToys.py
@@ -13,20 +13,16 @@ import glob
 def writeBashScript(box,sideband,fitmode,mg,mchi,xsec,nToys,nToysPerJob,t,doToys,doConvertToRoot,doFinalJob):
     pwd = os.environ['PWD']
     
-    fitResultsDir = "FitResults_SignalInjection2"
-    config = "config_summer2012/RazorInclusive2012_%s_hybrid.config"%fitmode
+    fitResultsDir = "FitResults_MCClosure"
+    config = "config_summer2012/avi.config"
 
     submitDir = "submit"
     
     xsecstring = str(xsec).replace(".","p")
     fitResultMap = {'T1bbbb':'%s/razor_output_T1bbbb_MG_%f_MCHI_%f_xsec_%s_%s.root'%(fitResultsDir,mg,mchi,xsecstring,box)}
     
-    if box == "TauTauJet" or box=="Jet" or box=="MultiJet":
-        mRmin = 400.
-        rMin = 0.5
-    else:
-        mRmin = 300.
-        rMin = 0.387298334621
+    mRmin = 300.
+    rMin = 0.387298334621
         
     label = "MR"+str(mRmin)+"_R"+str(rMin)
     datasetMap = {'T1bbbb':'SMS/T1bbbb_MG_%f_MCHI_%f_%s_%s.root'%(mg,mchi,label,box)}
@@ -38,19 +34,13 @@ def writeBashScript(box,sideband,fitmode,mg,mchi,xsec,nToys,nToysPerJob,t,doToys
     tagFR = ""
     tag3D = ""
 
-    if box=="MuEle" or box=="MuMu" or box=="EleEle" or box=="TauTauJet":
-        tagFR = "--fit-region=LowRsq_LowMR_HighMR"
-    else:
-        #tagFR = "--fit-region=LowRsq_LowMR_HighMR,LowRsq1b_LowMR1b_HighMR1b,LowRsq2b_LowMR2b_HighMR2b_LowRsq3b_LowMR3b_HighMR3b"
-        tagFR = "--fit-region=LowRsq2b_LowMR2b_HighMR2b_LowRsq3b_LowMR3b_HighMR3b"
-
-        
+    tagFR = "--fit-region=LowRsq_LowMR_HighMR"
+            
     if fitmode == "3D":
         tag3D = "--3D"
         
     tagPrintPlots = "--printPlots"
 
-        
     os.system("mkdir -p %s"%(submitDir)) 
     # prepare the script to run
     outputname = submitDir+"/submit_"+datasetName+"_"+fitmode+"_"+xsecstring+"_"+box+"_"+str(t)+".src"
@@ -58,7 +48,7 @@ def writeBashScript(box,sideband,fitmode,mg,mchi,xsec,nToys,nToysPerJob,t,doToys
     outputfile.write('#!/bin/bash\n')
     outputfile.write('cd %s \n'%pwd)
     outputfile.write('echo $PWD \n')
-    #outputfile.write('eval `scramv1 runtime -sh` \n')
+    outputfile.write('eval `scramv1 runtime -sh` \n')
     outputfile.write("source setup.sh\n")
     outputfile.write("mkdir -p %s; mkdir -p %s; mkdir -p %s \n"%(resultDir,toyDir,ffDir))
     if doToys:
@@ -66,9 +56,9 @@ def writeBashScript(box,sideband,fitmode,mg,mchi,xsec,nToys,nToysPerJob,t,doToys
     if doConvertToRoot:
         outputfile.write("python scripts/convertToyToROOT.py %s/frtoydata_%s --start=%i --end=%i -b \n" %(toyDir, box, int(t*nToysPerJob),int(t*nToysPerJob)+nToysPerJob))
     if doFinalJob:
-        #outputfile.write("rm %s.txt \n" %(toyDir))
-        #outputfile.write("ls %s/frtoydata*.root > %s.txt \n" %(toyDir, toyDir))
-        #outputfile.write("python scripts/expectedYield_sigbin.py 1 %s/expected_sigbin_%s.root %s %s.txt %s %s -b \n"%(ffDir, box, box, toyDir,tagFR,tag3D))
+        outputfile.write("rm %s.txt \n" %(toyDir))
+        outputfile.write("ls %s/frtoydata*.root > %s.txt \n" %(toyDir, toyDir))
+        outputfile.write("python scripts/expectedYield_sigbin.py 1 %s/expected_sigbin_%s.root %s %s.txt %s %s -b \n"%(ffDir, box, box, toyDir,tagFR,tag3D))
         outputfile.write("python scripts/makeToyPVALUE_sigbin.py %s %s/expected_sigbin_%s.root %s %s %s %s %s -b \n"%(box, ffDir, box, fitResultMap[datasetName], ffDir,tagFR,tag3D,tagPrintPlots))
         #outputfile.write("python scripts/make1DProj.py %s %s/expected_sigbin_%s.root %s %s -MC=%s %s %s %s -Label=%s_%s_%s -b \n"%(box,ffDir,box,fitResultMap[datasetName],ffDir,datasetName,tagFR,tag3D,tagPrintPlots,datasetName,xsecstring,box))
    
@@ -112,13 +102,12 @@ if __name__ == '__main__':
     else:
         boxNames = [sys.argv[2]]
 
-    
     if sys.argv[3]=='All':
-        xsecRange = [0.0, 0.001, 0.003, 0.005, 0.01, 0.04]
+        xsecRange = [0.0, 0.001, 0.003, 0.01, 0.05]
     else:
         xsecRange = [float(sys.argv[3])]
 
-    mg = 1225
+    mg = 1325
     mchi = 50
     
     nToysPerJob = int(nToys/nJobs)
@@ -162,9 +151,9 @@ if __name__ == '__main__':
                 if doFinalJob or doToys or doConvertToRoot:
                     outputname,ffDir,pwd = writeBashScript(box,sideband,fitmode,mg,mchi,xsec,nToys,nToysPerJob,t,doToys,doConvertToRoot,doFinalJob)
                     totalJobs+=1
-                    #time.sleep(3)
-                    #os.system("echo bsub -q "+queue+" -o "+pwd+"/"+ffDir+"/log_"+str(t)+".log source "+pwd+"/"+outputname)
-                    #os.system("bsub -q "+queue+" -o "+pwd+"/"+ffDir+"/log_"+str(t)+".log source "+pwd+"/"+outputname)
+                    time.sleep(3)
+                    os.system("echo bsub -q "+queue+" -o "+pwd+"/"+ffDir+"/log_"+str(t)+".log source "+pwd+"/"+outputname)
+                    os.system("bsub -q "+queue+" -o "+pwd+"/"+ffDir+"/log_"+str(t)+".log source "+pwd+"/"+outputname)
                     #os.system("source "+pwd+"/"+outputname)
                 
     print "TOTAL JOBS IS %i"%totalJobs
