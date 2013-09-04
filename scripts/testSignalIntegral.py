ecfdc4894871947416d66704be2e4424e7cabb08
Javier <jduarte@caltech.edu>
2013-09-03 18:02:28 -0700

    for debugging

--- scripts/testSignalIntegral.py
+++ scripts/testSignalIntegral.py
@@ -1,5 +1,5 @@
-from optparse import OptionParser
 import ROOT as rt
+from optparse import OptionParser
 from array import *
 import sys
 import RazorCombinedFit
@@ -13,20 +13,20 @@ def getBinning():
     return MRbins, Rsqbins
 
 if __name__ == '__main__':
-    
     # Load the signal p.d.f.
     # ---------------------------------------
     rt.gSystem.Load("../lib/libRazor")
     from ROOT import RooRazor2DSignal
 
-    from RazorCombinedFit.Framework import Config
-    cfg = Config.Config("/afs/cern.ch/work/s/salvati/CMSSW_6_1_1/src/RazorCombinedFit_wreece_101212_2011_style_fits/config_winter2012/MultiJet_All_fR1fR2fR3fR4_2012.cfg")
 
+    rt.RooRandom.randomGenerator().SetSeed(314159)
+
+    
     # Define variables
     # ---------------------------------------
-    MR = rt.RooRealVar("MR","MR",500,4000) 
-    Rsq = rt.RooRealVar("Rsq","Rsq",0.05,1.)
-    Ntot = rt.RooRealVar("Ntot","Ntot",0,1000000)
+    MR = rt.RooRealVar("MR","MR",600,500,4000) 
+    Rsq = rt.RooRealVar("Rsq","Rsq",0.1,0.05,1.)
+    Ntot = rt.RooRealVar("Ntot","Ntot",100,0,1000000)
 
     MR.setConstant(rt.kFALSE)
     Rsq.setConstant(rt.kFALSE)
@@ -47,90 +47,118 @@ if __name__ == '__main__':
     MR.setBinning(MRRooBins)
     Rsq.setBinning(RsqRooBins)
 
-    tfile = rt.TFile.Open("/afs/cern.ch/user/s/ssekmen/public/forWill/RzrMJSMS/SMS_T2tt_jan30_MR500.0_R0.22360679775_400.0_0.0_Ele.root")
+    tfile = rt.TFile.Open(sys.argv[1])
     nominal = tfile.Get("wHisto")
     jes = tfile.Get("wHisto_JESerr_pe")
     pdf = tfile.Get("wHisto_pdferr_pe")
     btag = tfile.Get("wHisto_btagerr_pe")
+    isr = tfile.Get("wHisto_isrerr_pe")
+    lep = tfile.Get("wHisto_leperr_pe")
     
     workspace = rt.RooWorkspace('newws')
     nominal.SetDirectory(0)
     jes.SetDirectory(0)
     pdf.SetDirectory(0)
     btag.SetDirectory(0)
-    
+    isr.SetDirectory(0)
+    lep.SetDirectory(0)
+
     RootTools.Utils.importToWS(workspace,nominal)
     RootTools.Utils.importToWS(workspace,jes)
     RootTools.Utils.importToWS(workspace,pdf)
     RootTools.Utils.importToWS(workspace,btag)
+    RootTools.Utils.importToWS(workspace,isr)
+    RootTools.Utils.importToWS(workspace,lep)
     
-    xJes = rt.RooRealVar("xJes","xJes",0.,-1.,1.) 
-    xPdf = rt.RooRealVar("xPdf","xPdf",0.,-1.,1.)
-    xBtag = rt.RooRealVar("xBtag","xBtag",0.,-1.,1.)
-
+    xJes = rt.RooRealVar("xJes","xJes",0.,-5.,5.) 
+    xPdf = rt.RooRealVar("xPdf","xPdf",0.,-5.,5.)
+    xBtag = rt.RooRealVar("xBtag","xBtag",0.,-5.,5.)
+    xIsr = rt.RooRealVar("xIsr","xIsr",0.,-5.,5.)
+    xLep = rt.RooRealVar("xLep","xLep",0.,-5.,5.)
+    
+    RootTools.Utils.importToWS(workspace,xJes)
+    RootTools.Utils.importToWS(workspace,xPdf)
+    RootTools.Utils.importToWS(workspace,xBtag)
+    RootTools.Utils.importToWS(workspace,xIsr)
+    RootTools.Utils.importToWS(workspace,xLep)
+    RootTools.Utils.importToWS(workspace,MR)
+    RootTools.Utils.importToWS(workspace,Rsq)
+    
     rt.RooMsgService.instance().getStream(1).addTopic(rt.RooFit.Integration)
-    razor = RooRazor2DSignal("razor","razor", MR, Rsq, workspace, "wHisto","wHisto_JESerr_pe","wHisto_pdferr_pe","wHisto_btagerr_pe", xJes, xPdf, xBtag)
-
-    ## add nuisance parameters to the PDF
-    xBtag_prime = rt.RooRealVar('xBtag_prime', 'xBtag_prime', 0)
-    xJes_prime = rt.RooRealVar('xJes_prime', 'xJes_prime', 0)
-    xPdf_prime = rt.RooRealVar('xPdf_prime', 'xPdf_prime', 0)
-    eff_prime = rt.RooRealVar('eff_prime', 'eff_prime', 0)
-    lumi_prime = rt.RooRealVar('lumi_prime', 'lumi_prime', 0)
-    nuisArgs = rt.RooArgSet(xBtag_prime, xJes_prime, xPdf_prime, eff_prime, lumi_prime)
-
-    lumi_value = rt.RooRealVar('lumi_value', 'lumi_value', 19300)
-    eff_value = rt.RooRealVar('eff_value', 'eff_value', 1.0)
-    lumi_uncert = rt.RooRealVar('lumi_uncert', 'lumi_uncert', 0.044)
-    eff_uncert = rt.RooRealVar('eff_uncert', 'eff_uncert', 0.1)
-    otherArgs = rt.RooArgSet(lumi_value, eff_value, lumi_uncert, eff_uncert )
-
-    sigma = rt.RooRealVar('sigma', 'sigma', 0.001)
-    poiArgs = rt.RooArgSet(sigma)
-
-    workspace.defineSet("nuisance", nuisArgs, True)
-    workspace.defineSet("other", otherArgs, True)
-    workspace.defineSet("poi", poiArgs, True)
-    workspace.factory("expr::lumi('@0 * pow( (1+@1), @2)', lumi_value, lumi_uncert, lumi_prime)")
-    workspace.factory("expr::eff('@0 * pow( (1+@1), @2)', eff_value, eff_uncert, eff_prime)")
-
-    ## now multiply the PDF by the already smeared nuisance parameters
-    nuisFile = rt.TFile.Open("NuisanceTree.root","read")
-    nuisTree = nuisFile.Get("nuisTree")
-    nuisTree.Draw('>>nuisElist','nToy==1','entrylist')
-    nuisElist = rt.gDirectory.Get('nuisElist')
-    nuisEntry = nuisElist.Next()
-    nuisTree.GetEntry(nuisEntry)
-    for var in RootTools.RootIterator.RootIterator(workspace.set('nuisance')):
-        # for each nuisance, grab gaussian distributed variables from ROOT tree
-        varVal = eval('nuisTree.%s'%var.GetName())
-        var.setVal(varVal)
-        print "NUISANCE PAR %s = %f"%(var.GetName(),var.getVal())
     
+    #razor = RooRazor2DSignal("razor","razor", MR, Rsq, workspace, "wHisto","wHisto_JESerr_pe","wHisto_pdferr_pe","wHisto_btagerr_pe", "wHisto_leperr_pe", "wHiso_isrerr_pe", xJes, xPdf, xBtag, xLep, xIsr)
+
+    razor = RooRazor2DSignal('razor','razor',\
+                                         workspace.var('MR'),workspace.var('Rsq'),\
+                                         workspace,\
+                                         nominal.GetName(),jes.GetName(),pdf.GetName(),btag.GetName(),lep.GetName(),isr.GetName(),\
+                                         workspace.var('xJes'),workspace.var('xPdf'),workspace.var('xBtag'),workspace.var('xLep'),workspace.var('xIsr'))
+    
+    #extrazor = rt.RooExtendPdf("extrazor","extrazor",razor,Ntot)
+    razor.Print("v")
+
+    RootTools.Utils.importToWS(workspace,razor)
+    #RootTools.Utils.importToWS(workspace,extrazor)
     workspace.Print("v")
-    
-    razordata = razor.generate(MRRsq,100)
 
-    #fr = razor.fitTo(razordata3D,rt.RooFit.Save())
-    #fr.Print("v")
+    workspace.var('xJes').setVal(0.0)
+    workspace.var('xPdf').setVal(0.0)
+    workspace.var('xBtag').setVal(0.0)
+    workspace.var('xIsr').setVal(0.0)
+    workspace.var('xLep').setVal(0.0)
+    workspace.var('xIsr').setConstant()
+    workspace.var('xLep').setConstant()
+    
+    razordata = razor.generate(MRRsq,10000)
+    errorArgSet = rt.RooArgSet()
+    errorArgSet.add(workspace.var('xJes'))
+    errorArgSet.add(workspace.var('xPdf'))
+    errorArgSet.add(workspace.var('xBtag'))
+    
+
+    fr = razor.fitTo(razordata,rt.RooFit.Save(),rt.RooFit.Constrained())
+    fr.Print("v")
+
+    par = 'xJes'
     
     MRframe  = MR.frame()
     razordata.plotOn(MRframe,rt.RooFit.Binning("MRRooBins"))
+    workspace.var(par).setVal(-1.0)
+    razor.plotOn(MRframe,rt.RooFit.LineColor(rt.kBlue-10))
+    workspace.var(par).setVal(1.0)
+    razor.plotOn(MRframe,rt.RooFit.LineColor(rt.kBlue-10))
+    workspace.var(par).setVal(0.0)
     razor.plotOn(MRframe)
     Rsqframe  = Rsq.frame()
     razordata.plotOn(Rsqframe,rt.RooFit.Binning("RsqRooBins"))
+    workspace.var(par).setVal(-1.0)
+    razor.plotOn(Rsqframe,rt.RooFit.LineColor(rt.kBlue-10))
+    workspace.var(par).setVal(1.0)
+    razor.plotOn(Rsqframe,rt.RooFit.LineColor(rt.kBlue-10))
+    workspace.var(par).setVal(0.0)
     razor.plotOn(Rsqframe)
+
+    hist = rt.TH2D("hist","hist",len(x)-1,x,len(y)-1,y)
+    razordata.fillHistogram(hist,rt.RooArgList(MR,Rsq))
 
     # Draw canvas
     # ---------------------------------------
     rt.gStyle.SetPalette(1)
     rt.gStyle.SetOptStat(0)
     c = rt.TCanvas("c","c",500,400)
-    c.Divide(2,1)
+    c.Divide(2,2)
     c.cd(1)
-    #rt.gPad.SetLogy()
+    rt.gPad.SetLogy(0)
     MRframe.Draw()
     c.cd(2)
-    #rt.gPad.SetLogy()
+    rt.gPad.SetLogy(0)
     Rsqframe.Draw()
+    c.cd(3)
+    rt.gPad.SetLogy(1)
+    rt.gPad.SetLogx(1)
+    hist.Draw("colz")
+    c.cd(4)
+    rt.gPad.SetLogy(1)
+    rt.gPad.SetLogx(1)
+    hist.Draw("surf2")
     c.Print("test.pdf")  
