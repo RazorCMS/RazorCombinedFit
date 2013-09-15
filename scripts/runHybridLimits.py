bea87a37e6cb36bfb4bc9b7f200eb47403460635
Javier <jduarte@caltech.edu>
2013-09-15 15:30:04 -0700

    attempt 2.0 at new hybrid limits

--- scripts/runHybridLimits.py
+++ scripts/runHybridLimits.py
@@ -190,7 +190,7 @@ if __name__ == '__main__':
         if gluinopoint < mg_lower or gluinopoint > mg_upper: continue
         gluinopoint = float(gluinopoint)
         xsecRange = getXsecRange(neutralinopoint,gluinopoint)
-        xsecRange = [5.,10.,15.,20.,25.]
+        xsecRange = [1., 5., 10., 15., 20.]
         for xsecpoint in xsecRange:
             for hypo in hypotheses:
                 for t in xrange(0,nJobs):
