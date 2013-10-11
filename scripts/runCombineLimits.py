1b9b2648d7afea4862edfb0b6ae2b4082ec46cb1
Javier <jduarte@caltech.edu>
2013-10-10 21:06:57 -0700

    ready for limits

--- scripts/runCombineLimits.py
+++ scripts/runCombineLimits.py
@@ -177,7 +177,7 @@ def writeBashScript(box,model,submitDir,neutralinopoint,gluinopoint,xsecpoint,hy
     outputfile.write("combine -M Asymptotic --saveWorkspace -n ${NAME}_%s_%s razor_combine_%s_${NAME}.txt\n"%(massPoint,box,box))
     
     outputfile.write("cp $WD/RazorCombinedFit/*.root %s \n"%combineDir)
-    outputfile.write("cd; rm -rf $WD\n")
+    outputfile.write("cd; rm -rf $TWD\n")
     
     outputfile.close
 
@@ -263,8 +263,8 @@ if __name__ == '__main__':
                     outputname,ffDir = writeBashScript(box,model,submitDir,neutralinopoint,gluinopoint,xsecpoint,hypo,t)
                     os.system("mkdir -p %s/%s"%(pwd,ffDir))
                     totalJobs+=1
-                    #time.sleep(3)
+                    time.sleep(3)
                     os.system("echo bsub -q "+queue+" -o "+pwd+"/"+ffDir+"/log_"+str(t)+".log source "+pwd+"/"+outputname)
-                    #os.system("bsub -q "+queue+" -o "+pwd+"/"+ffDir+"/log_"+str(t)+".log source "+pwd+"/"+outputname)
+                    os.system("bsub -q "+queue+" -o "+pwd+"/"+ffDir+"/log_"+str(t)+".log source "+pwd+"/"+outputname)
     print "Missing files = ", missingFiles
     print "Total jobs = ", totalJobs
