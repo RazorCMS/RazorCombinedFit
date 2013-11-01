7705e1fd959acecd6a7f24d852a9a149285e0c30
Javier <jduarte@caltech.edu>
2013-11-01 00:56:48 -0700

    david plots

--- scripts/makeDavidplots.py
+++ scripts/makeDavidplots.py
@@ -119,7 +119,7 @@ if __name__ == '__main__':
         a.setRealValue('MR',tree.MR)
         a.setRealValue('Rsq',tree.RSQ)
         a.setRealValue('nBtag',tree.btag_nom)
-        a.setRealValue('W',0.95*tree.WISR*tree.WLEP*tree.WPU*lumi*xsec/wall)
+        a.setRealValue('W',max(0.,0.95*tree.WISR*tree.WLEP*tree.WPU*lumi*xsec/wall))
 
         a.setRealValue('MRsq',(tree.MR-mr0)*(tree.RSQ-r0))
     
@@ -140,5 +140,5 @@ if __name__ == '__main__':
     data.plotOn(frame)
     #razPdf.plotOn(frame,rt.RooFit.Normalization(Ntot.getVal()))
     frame.Draw()
-    #c.SetLogy()
+    c.SetLogy()
     c.Print("MRsq.pdf")
